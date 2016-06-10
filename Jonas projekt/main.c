9ce7cc09ab00df9acbecc28bf75bd579fec7ae55
Jonas Ørnskov Nielsen <Jonas rnskov Nielsen>
2016-06-10 13:51:25 +0200

    ?

--- Jonas projekt/main.c
+++ Jonas projekt/main.c
@@ -4,9 +4,42 @@
 #include "ansi.h"
 
 void main() {
-	printf("Hello World!");
-	printFix(expand(cos(-512-256)));
-	window(2,3,8,9);
+	int a = 0xff80;
+	struct TVector *v, vector;
+	v = &vector;
+	
+	
+	initVector(v, 7,8);
+	/*printV(v);
+	printFix(expand(sin(128)));
+	printf("\n");
+	printFix(expand(cos(128)));
+	printf("\n");*/
+	rotate(v, 128);
+	printV(v);
+	/*printFix(expand(sin(-128)));
+	printf("\n");
+	printFix(expand(cos(-128)));
+	printf("\n");*/
+	rotate(v, 0xff80);
+	printV(v);
+	
+	/*printFix(expand(sin(0)));
+	printf("\n");
+	printFix(expand(sin(64)));
+	printf("\n");
+	printFix(expand(sin(-111)));
+	printf("\n");
+	printFix(expand(sin(923)));
+	printf("\n");
+	printFix(expand(cos(0)));
+	printf("\n");
+	printFix(expand(cos(64)));
+	printf("\n");
+	printFix(expand(cos(-111)));
+	printf("\n");
+	printFix(expand(cos(923)));
+	printf("\n");*/
 
 	do {} while(1);
 }
--- include/src/tri.c
+++ include/src/tri.c
@@ -4,16 +4,15 @@
 //Struktur til vektorer
 struct TVector {
 	long x,y;
-	long a,b;
 };
 
 //Initialiserer en vektor til (10,20)
-void initVector(struct TVector *v) {
-	v->x = 1 << 14;
-	v->y = 2 << 14;
+void initVector(struct TVector *v, long startX, long startY) {
+	v->x = startX << 14;
+	v->y = startY << 14;
 }
 
-//Printer en vektor pÃ¥ formatet "V=(x,y)
+//Printer en vektor på formatet "V=(x,y)
 void printV(struct TVector *v) {
 	printf("V=(");
 	printFix(expand(v->x));
