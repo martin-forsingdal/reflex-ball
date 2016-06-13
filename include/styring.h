f00faec5e5d41ff7eef39a9aa406c938a7d10ab2
coolpotato1 <s153612@student.dtu.dk>
2016-06-13 09:14:27 +0200

    Create styring.h

---
+++ include/styring.h
@@ -0,0 +1,11 @@
+#include "ansi.h"
+#include "tri.h"
+#include "LUT.h"
+#include "board.h"
+#include "charset.h"
+#include "fixpoint.h"
+
+void reflectBallWall(Tvector *vector);
+void startBall();
+void moveBall(Tvector *vector);
+void printBall(Tvector vector);
