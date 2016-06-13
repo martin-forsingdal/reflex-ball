302d253c7caec068e40f84acec6fceef8009ee33
martin forsingdal <martin forsingdal>
2016-06-13 14:25:56 +0200

    no message

--- include/src/Styring.c
+++ include/src/Styring.c
@@ -5,13 +5,15 @@
 #include "fixpoint.h"
 
 void startBall(struct TVector *vector){
-	initVector(vector,0xffffffff,0xffffffff,25,47); 
+	initVector(vector,0xffffffff,0xffffffff,30,15); 
 }
 
 void moveBall(struct TVector *vector){
-  vector->a=vector->a+vector->x;
-  vector->b=vector->b+vector->y;
-  }
+  	gotoxy(vector->a>>14,vector->b>>14);
+	printf("%c",' ');
+	vector->a=vector->a+vector->x;
+  	vector->b=vector->b+vector->y;
+  	}
   
 void reflectBallWall(struct TVector *vector){
   if((vector->b)>(2<<14)){
@@ -25,7 +27,6 @@ void printBall(struct TVector *vector){
   int x,y;
   x=(int) ((vector->a+0x1000)>>14);
   y=(int) ((vector->b+0x1000)>>14);
-  printf("V=(%ld,%ld),%d,%d",(vector->a)>>14,(vector->b)>>14,x,y);
   gotoxy(x,y);
   printf("%c", 'o');
 }
