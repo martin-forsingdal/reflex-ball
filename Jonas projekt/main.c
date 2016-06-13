7676a3e21f4ff0decd1bde2cf38bc4adba84ce47
Jonas Ørnskov Nielsen <Jonas rnskov Nielsen>
2016-06-13 16:05:06 +0200

    no message

--- Jonas projekt/main.c
+++ Jonas projekt/main.c
@@ -5,53 +5,6 @@
 #include "styring.h"
 
 void main() {
-<<<<<<< Updated upstream
-	
-<<<<<<< Updated upstream
-	struct TVector v;
-	startBall(&v);
-	
-	
-	
-	
-=======
-	long a,b;
-	a = 4 << 14;
-	b = 9 << 14;
-	
->>>>>>> Stashed changes
-	/*initVector(v, 7,8,9,8);
-	/*printV(v);
-	printFix(expand(sin(128)));
-	printf("\n");
-	printFix(expand(cos(128)));
-	printf("\n");
-	rotate(v, 128);
-	printV(v);
-	printFix(expand(sin(-128)));
-	printf("\n");
-	printFix(expand(cos(-128)));
-	printf("\n");
-	rotate(v, 0xff80);
-	printV(v);*/
-	
-	/*printFix(expand(sin(0)));
-	printf("\n");
-	printFix(expand(sin(64)));
-	printf("\n");
-	printFix(expand(sin(-111)));
-	printf("\n");
-	printFix(expand(sin(923)));
-	printf("\n");
-	printFix(expand(cos(0)));
-	printf("\n");
-	printFix(expand(cos(64)));
-	printf("\n");
-	printFix(expand(cos(-111)));
-	printf("\n");
-	printFix(expand(cos(923)));
-	printf("\n");*/
-=======
 	char i = 0x00;
 	struct TVector v;
 	startBall(&v);
@@ -66,7 +19,6 @@ void main() {
 	}
 	printf("V = ([%ld, %ld], [%ld, %ld])\n", v.x>>14, v.y>>14, v.a>>14, v.b>>14);
 
->>>>>>> Stashed changes
 
 	do {} while(1);
 }
