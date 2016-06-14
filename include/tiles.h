4e065bbc552382200a0e78c304501832466a92ff
Jonas Ørnskov Nielsen <Jonas rnskov Nielsen>
2016-06-14 11:32:22 +0200

    Header fil tiles.h opdateret.

--- include/tiles.h
+++ include/tiles.h
@@ -1,7 +1,7 @@
 #ifndef tiles_h
 #define tiles_h
 
-
+void initTileArray(char tilesNumber[11][24]);
 void levelOne(char tilesOne[11][24]);
 void levelTwo(char tilesTwo[11][24]);
 void levelThree(char tilesThree[11][24]);
