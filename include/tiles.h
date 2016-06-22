df2430a6bab1c061ea3e96cb347812a7690e8fa3
Henning Frederiksen <Henning Frederiksen>
2016-06-22 11:05:51 +0200

    no message

--- include/tiles.h
+++ include/tiles.h
@@ -1,15 +1,15 @@
 #ifndef tiles_h
 #define tiles_h
 
-void initTileArray(unsigned char tilesNumber[11][24]);
-void levelOne(unsigned char tilesOne[11][24]);
-void levelTwo(unsigned char tilesTwo[11][24]);
-void levelThree(unsigned char tilesThree[11][24]);
-void levelChoose(unsigned char levelArray[11][24], char level);
-void printLevel(unsigned char levelArray[11][24]);
-void tileCheck(unsigned char levelArray[11][24], struct TVector *vector, int *points);
-void tileUpdate(unsigned char levelArray[11][24], int y, int x, int *points);
-void printTileArray(unsigned char levelArray[11][24]);
+void initTileArray(unsigned char tilesNumber[11][15]);
+void levelOne(unsigned char tilesOne[11][15]);
+void levelTwo(unsigned char tilesTwo[11][15]);
+void levelThree(unsigned char tilesThree[11][15]);
+void levelChoose(unsigned char levelArray[11][15], char level);
+void printLevel(unsigned char levelArray[11][15]);
+void tileCheck(unsigned char levelArray[11][15], struct TVector *vector, int *points);
+void tileUpdate(unsigned char levelArray[11][15], int y, int x, int *points);
+void printTileArray(unsigned char levelArray[11][15]);
 
 
 #endif	// tiles_h
